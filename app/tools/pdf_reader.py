6a3785dd05f4c8b021ac56571e0243c9811ede8b
Monika BV <monikavishu1609@gmail.com>
2026-05-22 12:35:41 +0530

    Rag merged with froant end and fixed main.py file

---
+++ app/tools/pdf_reader.py
@@ -0,0 +1,63 @@
+from pypdf import PdfReader
+
+import re
+
+from app.rag.processing.text_chunker import (
+    chunk_text
+)
+
+
+def read_pdf(
+    file_path: str
+):
+
+    try:
+
+        # Open PDF
+        reader = PdfReader(
+            file_path
+        )
+
+        text = ""
+
+        # Read all pages
+        for page in reader.pages:
+
+            page_text = (
+                page.extract_text()
+            )
+
+            # Remove weird spacing between characters
+            page_text = re.sub(
+
+                r'(?<=\w)\s(?=\w)',
+
+                '',
+
+                page_text
+            )
+
+            # Normalize spaces
+            page_text = " ".join(
+                page_text.split()
+            )
+
+            text += (
+                page_text
+                + "\n"
+            )
+
+        # Create chunks
+        chunks = chunk_text(
+            text
+        )
+
+        return chunks
+
+    except Exception as e:
+
+        print(e)
+
+        return (
+            "PDF reading failed."
+        )
