e99704354f3faf46e9b7536a6bd3eb73bc06fad2
Monika BV <monikavishu1609@gmail.com>
2026-05-25 12:51:43 +0530

    Integrated Jina embedding API for deployment optimization

--- app/rag/retrieval/embedding_generator.py
+++ app/rag/retrieval/embedding_generator.py
@@ -1,19 +1,56 @@
-from sentence_transformers import (
-    SentenceTransformer
+import requests
+
+import os
+
+from dotenv import (
+    load_dotenv
 )
 
-# Load embedding model
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2"
-)
+load_dotenv()
 
 
 def generate_embeddings(
     chunks: list
 ):
 
-    embeddings = model.encode(
-        chunks
+    url = (
+        "https://api.jina.ai/v1/embeddings"
     )
+
+    headers = {
+
+        "Content-Type":
+        "application/json",
+
+        "Authorization":
+        f"Bearer {os.getenv('JINA_API_KEY')}"
+    }
+
+    data = {
+
+        "model":
+        "jina-embeddings-v2-base-en",
+
+        "input":
+        chunks
+    }
+
+    response = requests.post(
+
+        url,
+
+        headers=headers,
+
+        json=data
+    )
+
+    result = response.json()
+
+    embeddings = [
+
+        item["embedding"]
+
+        for item in result["data"]
+    ]
 
     return embeddings
